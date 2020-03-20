c30fd261a2a4ba7e7f66b09e30aa12eb8da4c920
Skyner <sacwks@gmailcom>
2020-03-20 13:33:12 -0400

    Resulto el conveniente

--- inc/funciones/bd.php
+++ inc/funciones/bd.php
@@ -2,7 +2,7 @@
 
 // credenciales de la base de datos
 define('DB_USUARIO', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 define('DB_HOST', 'localhost');
 define('DB_NOMBRE', 'agendaphp');
 
